66cd48fb8d8f69866e0795cc7b91dc88524193cd
Saeed Taheri <saeed.taheri@gmail.com>
2015-07-29 10:47:01 +0430

    Initial Commit

---
+++ ShahrYar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShahrYar
+//
+//  Created by Saeed Taheri on 2015/7/29.
+//  Copyright (c) 2015 Saeed Taheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShahrYar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShahrYar
+//
+//  Created by Saeed Taheri on 2015/7/29.
+//  Copyright (c) 2015 Saeed Taheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
